48ac30b261c7e1fd05c67170db90463cae9a8f1b
Aisha <ayeshashafique123@gmail.com>
2024-02-05 11:12:14 +0500

    A very basic frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,11 +19,17 @@ export async function signup(req, res) {
     return res.status(409).json({ error: "Username already exists" });
   }
 
+  const token = jwt.sign({ username }, process.env.JWT_PRIVATE_KEY, {
+    expiresIn: "1h",
+  });
+
+
   const user = new User({ username, email, password });
 
   try {
     user.save();
-    res.status(200).json({ message: "User created" });
+    res.status(200).json({ username, token });
+    // res.status(200).json({ message: "User created" });
   } catch (err) {
     console.log(err.message);
     res.status(500).json({ error: "Server error" });
