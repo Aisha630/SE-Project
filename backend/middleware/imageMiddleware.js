4a14e64d0b19203963ce2b07f621bc2d683e210a
Hafsa Akbar <25100083@lums.edu.pk>
2024-03-27 16:14:06 +0500

    image middleware

---
+++ backend/middleware/imageMiddleware.js
@@ -0,0 +1,20 @@
+import multer from "multer";
+import path from "path";
+
+export default multer({
+  limits: { fileSize: 2000000 },
+  fileFilter: (_, file, cb) => {
+    const filetypes = /jpeg|jpg|png|webp/;
+
+    const extname = filetypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    } else {
+      cb("Error: Images Only!");
+    }
+  },
+}).array("images", 5);
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,6 +1,5 @@
 import express from "express";
-import multer from "multer";
-import path from "path";
+import uploadImage from "../middleware/imageMiddleware.js";
 
 import {
   getAllProducts,
@@ -13,29 +12,11 @@ import {
 
 const router = express.Router();
 
-const upload = multer({
-  limits: { fileSize: 2000000 },
-  fileFilter: (_, file, cb) => {
-    const filetypes = /jpeg|jpg|png|webp/;
-
-    const extname = filetypes.test(
-      path.extname(file.originalname).toLowerCase()
-    );
-    const mimetype = filetypes.test(file.mimetype);
-
-    if (mimetype && extname) {
-      return cb(null, true);
-    } else {
-      cb("Error: Images Only!");
-    }
-  },
-}).array("images", 5);
-
 router.get("/shop", getAllProducts);
 router.get("/shop/:id", getProduct);
 router.delete("/shop/:id", deleteProduct);
 
-router.post("/sell", upload, addProduct);
+router.post("/sell", uploadImage, addProduct);
 router.get("/filter", filterProducts);
 router.get("/latest", fetchLatest);
 
