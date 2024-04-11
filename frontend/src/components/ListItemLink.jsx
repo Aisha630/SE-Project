833266eaac7f025d93725cb92acfedb696177b0a
Bilal <25100266@lums.edu.pk>
2024-04-11 17:28:07 +0500

    pageon fix

--- frontend/src/components/ListItemLink.jsx
+++ frontend/src/components/ListItemLink.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ListItemButton, ListItemIcon, Typography, Avatar } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 
-const ListItemLink = ({ text, Icon, to, imgSrc, TypoStyles, ButtonStyles, onClick, pageOn }) => {
+const ListItemLink = ({ text, Icon, to, imgSrc, TypoStyles, ButtonStyles, onClick, pageon }) => {
     const commonStyles = {
         minWidth: { xs: '30px', sm: '40px', md: '50px' },
         fontSize: { xs: '0.7rem', sm: '0.8rem', md: '0.9rem' },
@@ -11,7 +11,7 @@ const ListItemLink = ({ text, Icon, to, imgSrc, TypoStyles, ButtonStyles, onClic
     };
 
     return (
-        <ListItemButton component={RouterLink} to={to} onClick={onClick ? onClick : () => { }} sx={{ '& .MuiListItemIcon-root, & .MuiTypography-root': commonStyles, ...ButtonStyles, maxWidth:'calc(100% - 8px)' }} selected={pageOn===text}>
+        <ListItemButton component={RouterLink} to={to} onClick={onClick ? onClick : () => { }} sx={{ '& .MuiListItemIcon-root, & .MuiTypography-root': commonStyles, ...ButtonStyles, maxWidth:'calc(100% - 8px)' }} selected={pageon===text}>
             {Icon && <ListItemIcon><Icon /></ListItemIcon>}
             {imgSrc && <Avatar src={imgSrc} variant="square" sx={{ width: "5%", height: "5%" }} />}
             <Typography variant="subtitle1" gutterBottom sx={{ ...TypoStyles, ...commonStyles }}>
